6ba65da033060eec79eb0d2d12782dd95873f317
Talabs8 <talabiodunn@gmail.com>
2025-01-27 11:02:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let html = document.getElementById('html')
+let btn = document.getElementById('btn')
+
+
+
+
+btn.addEventListener('click', function(){
+    html.classList.toggle('dark')
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // darkMode: 'media',
+
+  darkMode: 'class',
+
+  content: ["*"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
